c132dc3f94aade13886df7694d5c954e530251a4
NameNotFound0 <aplascenm@gmail.com>
2024-07-21 22:45:43 -0400

    Minor changes Artist Controller

--- controllers/artistController.js
+++ controllers/artistController.js
@@ -32,8 +32,6 @@ const createArtist = asyncHandler(async(req, res) =>{
 		const artist = await Artist.create(req.body)
 		res.status(200).json(artist);
 	}catch(error){
-		console.log(error.message);
-		//res.status(500).json({message: error.message})
 		res.status(500);
 		throw new Error(error.message);
 	}
